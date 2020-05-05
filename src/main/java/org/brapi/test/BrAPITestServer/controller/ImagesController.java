bb3509417e7e8e013dc8b11378fb7e9f13f0f60c
Peter Selby <ps664@cornell.edu>
2020-05-05 18:10:58 -0400

    image storage fixes

--- src/main/java/org/brapi/test/BrAPITestServer/controller/ImagesController.java
+++ src/main/java/org/brapi/test/BrAPITestServer/controller/ImagesController.java
@@ -7,9 +7,13 @@ import javax.validation.Valid;
 import org.brapi.test.BrAPITestServer.exceptions.BrAPIServerException;
 import org.brapi.test.BrAPITestServer.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -53,8 +57,8 @@ public class ImagesController extends BrAPIController implements ImagesApi {
 
 	@CrossOrigin
 	@Override
-	public ResponseEntity<ImageResponse> imagesImageDbIdGet(@PathVariable("imageDbId") String imageDbId, String authorization)
-			throws BrAPIServerException {
+	public ResponseEntity<ImageResponse> imagesImageDbIdGet(@PathVariable("imageDbId") String imageDbId,
+			String authorization) throws BrAPIServerException {
 		Image data = imageService.getImage(imageDbId);
 
 		ImageResponse response = new ImageResponse();
@@ -64,10 +68,22 @@ public class ImagesController extends BrAPIController implements ImagesApi {
 	}
 
 	@CrossOrigin
+	@Override
+	public ResponseEntity<byte[]> imagesImageDbIdContentGet(@PathVariable("imageDbId") String imageDbId,
+			@PathVariable("imageName") String imageName, String authorization) throws BrAPIServerException {
+		byte[] data = imageService.getImageData(imageDbId);
+		Image image = imageService.getImage(imageDbId);
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.parseMediaType(image.getMimeType()));
+	    headers.setCacheControl(CacheControl.noCache().getHeaderValue());
+		return new ResponseEntity<byte[]>(data, headers, HttpStatus.OK);
+	}
+
+	@CrossOrigin
 	@PreAuthorize("hasAuthority('ADMIN')")
 	@Override
-	public ResponseEntity<ImageResponse> imagesImageDbIdImagecontentPut(@PathVariable("imageDbId") String imageDbId, @Valid @RequestBody byte[] imageData,
-			String authorization) throws BrAPIServerException {
+	public ResponseEntity<ImageResponse> imagesImageDbIdImagecontentPut(@PathVariable("imageDbId") String imageDbId,
+			@Valid @RequestBody byte[] imageData, String authorization) throws BrAPIServerException {
 		Image data = imageService.saveImageData(imageDbId, imageData);
 
 		ImageResponse response = new ImageResponse();
@@ -79,8 +95,8 @@ public class ImagesController extends BrAPIController implements ImagesApi {
 	@CrossOrigin
 	@PreAuthorize("hasAuthority('ADMIN')")
 	@Override
-	public ResponseEntity<ImageResponse> imagesImageDbIdPut(@PathVariable("imageDbId") String imageDbId, @Valid @RequestBody NewImageRequest request,
-			String authorization) throws BrAPIServerException {
+	public ResponseEntity<ImageResponse> imagesImageDbIdPut(@PathVariable("imageDbId") String imageDbId,
+			@Valid @RequestBody NewImageRequest request, String authorization) throws BrAPIServerException {
 		Image data = imageService.updateImageMetaData(imageDbId, request);
 
 		ImageResponse response = new ImageResponse();
--- src/main/java/org/brapi/test/BrAPITestServer/model/entity/ImageEntity.java
+++ src/main/java/org/brapi/test/BrAPITestServer/model/entity/ImageEntity.java
@@ -15,14 +15,14 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="image")
-public class ImageEntity extends BaseEntity{
+@Table(name = "image")
+public class ImageEntity extends BaseEntity {
 	@ElementCollection
 	private List<String> additionalInfo;
 	@Column
 	private byte[] imageData;
-	@ManyToOne(cascade=CascadeType.DETACH)
-	private ObservationUnitEntity observationUnit;	
+	@ManyToOne(cascade = CascadeType.DETACH)
+	private ObservationUnitEntity observationUnit;
 	@Column
 	private String name;
 	@Column
@@ -54,111 +54,146 @@ public class ImageEntity extends BaseEntity{
 
 	public Map<String, String> getAdditionalInfo() {
 		Map<String, String> map = new HashMap<>();
-		for(String entry: additionalInfo) {
-			String[] splitEntry = entry.split("-:-");
-			map.put(splitEntry[0], splitEntry[1]);
+		if (this.additionalInfo != null) {
+			for (String entry : additionalInfo) {
+				String[] splitEntry = entry.split("-:-");
+				map.put(splitEntry[0], splitEntry[1]);
+			}
 		}
 		return map;
 	}
+
 	public void setAdditionalInfo(Map<String, String> additionalInfo) {
 		this.additionalInfo = new ArrayList<>();
-		for(Entry<String, String> entry: additionalInfo.entrySet()) {
+		for (Entry<String, String> entry : additionalInfo.entrySet()) {
 			this.additionalInfo.add(entry.getKey() + "-:-" + entry.getValue());
 		}
 	}
+
 	public String getCopyright() {
 		return copyright;
 	}
+
 	public void setCopyright(String copyright) {
 		this.copyright = copyright;
 	}
+
 	public Date getTimeStamp() {
 		return timeStamp;
 	}
+
 	public void setTimeStamp(Date timeStamp) {
 		this.timeStamp = timeStamp;
 	}
+
 	public Float getLongitude() {
 		return longitude;
 	}
+
 	public void setLongitude(Float longitude) {
 		this.longitude = longitude;
 	}
+
 	public Float getLatitude() {
 		return latitude;
 	}
+
 	public void setLatitude(Float latitude) {
 		this.latitude = latitude;
 	}
+
 	public byte[] getImageData() {
 		return imageData;
 	}
+
 	public void setImageData(byte[] imageData) {
 		this.imageData = imageData;
 	}
+
 	public ObservationUnitEntity getObservationUnit() {
 		return observationUnit;
 	}
+
 	public void setObservationUnit(ObservationUnitEntity observationUnit) {
 		this.observationUnit = observationUnit;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getImageFileName() {
 		return imageFileName;
 	}
+
 	public void setImageFileName(String imageFileName) {
 		this.imageFileName = imageFileName;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getImageURL() {
 		return imageURL;
 	}
+
 	public void setImageURL(String imageURL) {
 		this.imageURL = imageURL;
 	}
+
 	public Integer getImageFileSize() {
 		return imageFileSize;
 	}
+
 	public void setImageFileSize(Integer imageFileSize) {
 		this.imageFileSize = imageFileSize;
 	}
+
 	public Integer getImageHeight() {
 		return imageHeight;
 	}
+
 	public void setImageHeight(Integer imageHeight) {
 		this.imageHeight = imageHeight;
 	}
+
 	public Integer getImageWidth() {
 		return imageWidth;
 	}
+
 	public void setImageWidth(Integer imageWidth) {
 		this.imageWidth = imageWidth;
 	}
+
 	public String getImageType() {
 		return imageType;
 	}
+
 	public void setImageType(String imageType) {
 		this.imageType = imageType;
 	}
+
 	public String getDescriptiveOntologyTerms() {
 		return descriptiveOntologyTerms;
 	}
+
 	public void setDescriptiveOntologyTerms(String descriptiveOntologyTerms) {
 		this.descriptiveOntologyTerms = descriptiveOntologyTerms;
 	}
+
 	public String getObservationDbIds() {
 		return observationDbIds;
 	}
+
 	public void setObservationDbIds(String observationDbIds) {
 		this.observationDbIds = observationDbIds;
 	}
