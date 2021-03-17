7c4e5a9adff48b06c929ab1367b7c47935942472
Peter Selby <ps664@cornell.edu>
2021-03-17 14:26:05 -0400

    validating oauth tokens woo

--- src/main/java/org/brapi/test/BrAPITestServer/auth/BrapiTestServerJWTAuthFilter.java
+++ src/main/java/org/brapi/test/BrAPITestServer/auth/BrapiTestServerJWTAuthFilter.java
@@ -1,12 +1,17 @@
 package org.brapi.test.BrAPITestServer.auth;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringReader;
 import java.net.URL;
 import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.EncodedKeySpec;
 import java.security.spec.InvalidKeySpecException;
@@ -20,7 +25,9 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.DatatypeConverter;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpEntity;
 import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
@@ -28,7 +35,6 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.springframework.http.HttpStatus;
@@ -114,14 +120,15 @@ public class BrapiTestServerJWTAuthFilter extends BasicAuthenticationFilter {
 		try {
 			String token = request.getHeader("Authorization");
 			if (token != null) {
+				token = token.replaceFirst("Bearer ", "");
 				RSAPublicKey pubKey = getPublicKey(OIDC_DISCOVERY_URL);
 								
 				Algorithm algorithm = Algorithm.RSA256(pubKey, null);
 				JWTVerifier verifier = JWT.require(algorithm)
-						.withIssuer("auth0")
+						.withIssuer("https://auth.brapi.org/auth/realms/brapi")
 						.build(); 
 				DecodedJWT jwt = verifier.verify(token);
-				return "";
+				return jwt.getClaim("email").asString();
 			}
 			return null;
 		} catch (Exception e) {
@@ -143,28 +150,38 @@ public class BrapiTestServerJWTAuthFilter extends BasicAuthenticationFilter {
 	}
 
 	private RSAPublicKey getPublicKey(String discoveryURL) {
-		RSAPublicKey pubKey = null;
 		try {
 			JsonNode discovery = (new ObjectMapper()).readTree(new URL(discoveryURL));
 			String jwksURL = discovery.findValue("jwks_uri").asText();
 			JsonNode jwks = (new ObjectMapper()).readTree(new URL(jwksURL));
 			String keyVal = jwks.findValue("keys").get(0).findValue("x5c").get(0).asText();
-			keyVal = "-----BEGIN CERTIFICATE-----\n" + keyVal + "\n-----END CERTIFICATE-----";
+			//keyVal = "-----BEGIN CERTIFICATE-----\n" + keyVal + "\n-----END CERTIFICATE-----";
 			
-            KeyFactory kf = KeyFactory.getInstance("RSA");
-            EncodedKeySpec keySpec = new X509EncodedKeySpec(keyVal.getBytes());
-            pubKey = (RSAPublicKey) kf.generatePublic(keySpec);
+//			byte[] encoded = Base64.decodeBase64(keyVal);
+//			
+//            KeyFactory kf = KeyFactory.getInstance("RSA");
+//		    RSAPublicKey pubKey = (RSAPublicKey) kf.generatePublic(new X509EncodedKeySpec(encoded));
+//			
+//			return pubKey;		    
 			
-			return pubKey;
+			String certb64 = keyVal;
+			byte[] certder = Base64.decodeBase64(certb64);
+			InputStream certstream = new ByteArrayInputStream(certder);
+			Certificate cert = CertificateFactory.getInstance("X.509").generateCertificate(certstream);
+			RSAPublicKey pubKey = (RSAPublicKey) cert.getPublicKey();
+			return pubKey;	
 		} catch (IOException e) {
 			e.printStackTrace();
-			return pubKey;
-		} catch (InvalidKeySpecException e) {
+			return null;
+//		} catch (InvalidKeySpecException e) {
+//			e.printStackTrace();
+//			return null;
+//		} catch (NoSuchAlgorithmException e) {
+//			e.printStackTrace();
+//			return null;
+		} catch (CertificateException e) {
 			e.printStackTrace();
-			return pubKey;
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-			return pubKey;
+			return null;
 		}
 	}
 }
