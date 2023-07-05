82a0c84d87116daed5aeef5b5f6ee430ea81b13e
Peter Selby <ps664@cornell.edu>
2023-07-05 18:48:07 -0400

    pedigree update fix

---
+++ src/main/java/org/brapi/test/BrAPITestServer/repository/germ/PedigreeEdgeRepository.java
@@ -0,0 +1,8 @@
+package org.brapi.test.BrAPITestServer.repository.germ;
+
+import org.brapi.test.BrAPITestServer.model.entity.germ.PedigreeEdgeEntity;
+import org.brapi.test.BrAPITestServer.repository.BrAPIRepository;
+
+public interface PedigreeEdgeRepository extends BrAPIRepository<PedigreeEdgeEntity, String> {
+	
+}
